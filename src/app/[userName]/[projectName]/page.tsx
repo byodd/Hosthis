babbf9445533418d10476b79d62e44b5c8ac9c12
gatta <gatta.ba@etu.univ-lyon1.fr>
2024-11-09 10:39:47 +0100

    Transformation du formulaire de commande en composant

--- src/app/[userName]/[projectName]/page.tsx
+++ src/app/[userName]/[projectName]/page.tsx
@@ -4,18 +4,15 @@ import { useSession, signIn } from "next-auth/react";
 import Image from "next/image";
 import GitHubLogoIcon from "../../../../public/svg/github-logo.svg";
 import Header from "../../components/Header";
-import { createProject } from "../../utils/project.service";
 import { getProject } from "@/app/utils/project.service";
 import { useParams } from "next/navigation";
 import { GithubProject } from "@/app/types/projects.type";
 
+import CommandForm from "@/app/components/project/CommandForm";
+
 export default function Project() {
   const { data: session } = useSession();
   const [gitHubProject, setGitHubProject] = useState<GithubProject | null>();
-
-  const [installCommand, setInstallCommand] = useState("");
-  const [buildCommand, setBuildCommand] = useState("");
-  const [launchCommand, setLaunchCommand] = useState("");
 
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string>();
@@ -23,7 +20,6 @@ export default function Project() {
   const params = useParams();
 
   const userEmail: string | null | undefined = session?.user?.email;
-  const projectUrl: string | null | undefined = gitHubProject?.html_url;
 
   useEffect(() => {
     if (userEmail) {
@@ -32,68 +28,15 @@ export default function Project() {
         .catch((error) => {
           console.error("Fetching GitHub project failed:", error);
           setError("La récupération du projet GitHub a échoué");
-        }
-        );
+        });
     }
     setLoading(false);
   }, [userEmail, params.userName, params.projectName]);
 
-  function sendInfo() {
-    if (projectUrl && userEmail)
-      createProject(projectUrl, installCommand, buildCommand, launchCommand, userEmail).then(() => {
-        console.log("Project created");
-      }).catch((error) => {
-        console.error("Failed to create the project:", error);
-      }
-      );
-  }
-
   const content = session ? (
-    <div className="flex flex-col items-center">
-      <p className="text-2xl">
-        Tu veux héberger <span className="font-bold">{gitHubProject?.name}</span> ?
-      </p>
-      <p className="text-lg mb-4">
-        Remplis les champs ci-dessous pour créer le projet.
-      </p>
-      <form className="flex flex-col w-full mt-3">
-        <label>Commande d&apos;installation des packages</label>
-        <input
-          type="text"
-          placeholder="npm install"
-          className="border-gray-300 border py-3 px-6 rounded-md mb-2"
-          value={installCommand}
-          onChange={(e) => setInstallCommand(e.target.value)}
-        />
-        <label>Commande de build</label>
-        <input
-          type="text"
-          placeholder="npm run build"
-          className="border-gray-300 border py-3 px-6 rounded-md mb-2"
-          value={buildCommand}
-          onChange={(e) => setBuildCommand(e.target.value)}
-        />
-        <label>Commande de lancement</label>
-        <input
-          type="text"
-          placeholder="npm start"
-          value={launchCommand}
-          className="border-gray-300 border py-3 px-6 rounded-md mb-2"
-          onChange={(e) => setLaunchCommand(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="bg-[#023246] text-white py-3 px-6 rounded-md my-2 hover:bg-[#224f66] duration-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
-          disabled={!installCommand || !buildCommand || !launchCommand}
-          onClick={(e) => {
-            e.preventDefault();
-            sendInfo();
-          }}
-        >
-          Créer le projet
-        </button>
-      </form>
-    </div >
+    gitHubProject ? (
+      <CommandForm project={gitHubProject}></CommandForm>
+    ) : null
   ) : (
     <button
       className="bg-none flex flex-row border-gray-300 border py-3 px-6 rounded-md mb-2 hover:bg-gray-100 duration-300"
@@ -108,13 +51,8 @@ export default function Project() {
     <div className="h-full overflow-hidden">
       <Header />
       <div className="h-full flex flex-col items-center justify-center">
-        {error && <p className="text-red-500"
-        >❌{error}</p>}
-        {
-          loading ? <p>Chargement...</p> :
-            error ? null :
-              content
-        }
+        {error && <p className="text-red-500">❌{error}</p>}
+        {loading ? <p>Chargement...</p> : error ? null : content}
       </div>
     </div>
   );
